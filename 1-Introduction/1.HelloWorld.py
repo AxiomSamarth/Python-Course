7fb3f2e9f0e22e66e06636ce433e3f7abe3abc6e
Samarth Deyagond <deyagondsamarth@gmail.com>
2016-08-04 22:35:58 +0530

    Update 1.HelloWorld.py

--- 1-Introduction/1.HelloWorld.py
+++ 1-Introduction/1.HelloWorld.py
@@ -2,7 +2,12 @@
 
 #this is how we comment in python
 
-#there is no multiline comments in pyhton, we need to prefix # symbols in each line
+"""
+This is 
+multiline 
+commenting
+
+"""
 
 
 print "Hello World !"
